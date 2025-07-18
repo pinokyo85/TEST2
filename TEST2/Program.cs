6a4bc49ff9ac3fb5fe07e1ec3ab8b31d4eb201bc
Mazen Fady <mazenfady85@gmail.com>
2025-07-18 17:24:21 +0300

    Add project files.

---
+++ TEST2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Demo 
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            int[] id = new int[];
+            string[] title = new string[];
+            string[] description = new string[];
+            
+           
+
+        }
+
+        public static void Getid() 
+        {
+            Console.WriteLine("Enter number of tasks");
+            int count =int.Parse(Console.ReadLine()!);
+        }
+    
+        public static void Points(int[] id, string[] title, string[]description)
+        {
+            for (int i = 0; i < id.Length; i++)
+            { 
+                conole.
+            }
+
+       
+        }
+    }
+
+}
